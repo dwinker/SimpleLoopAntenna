78d3cc7bbec147bfafedbd9b7b280a6b52505e25
Dan Winker <dwinker@visi.com>
2017-11-12 19:40:35 -0600

    Added 75 ohm transmission line impedence matching to 50 ohm.

--- SimpleLoopFactory.pl
+++ SimpleLoopFactory.pl
@@ -6,20 +6,13 @@ use strict;
 # c/170 MHz = 1.76 meters.
 # Small loop ~ 10% wavelength at 1.76 meters -> 0.028 meters radius
 # Large loop ~  1  wavelength at 2.54 meters -> 0.400 meters radius
-my $ns   =  4;      # Number of segments into which the arc will be divided.
-my $rada = 0.400;   # Arc radius (center is the origin and the axis is the y axis).
-my $ang1 = -88.0;   # Angle of first end of the arc measured from the x axis in a left-hand direction about the y axis (degrees).
-my $ang2 = 268.0;   # Angle of the second end of the arc.
-my $rad  = 0.006;   # Wire radius.
+my $ns   = 31;      # Number of segments into which the arc will be divided. Should be an odd number.
+my $rada =  0.333;   # Arc radius (center is the origin and the axis is the y axis). 0.333 for 146.67 MHz.
+my $ang1 = -270.0;   # Angle of first end of the arc measured from the x axis in a left-hand direction about the y axis (degrees).
+my $ang2 =   90.0;   # Angle of the second end of the arc.
+my $rad  =  0.001;   # Wire radius. 0.001 is approximately 14 AWG.
 
-my $pi = 4 * atan2(1, 1);   # 3.14159...
-my $x1 = -1 * $rada * cos($ang1 * $pi / 180.0);
-my $z1 =      $rada * sin($ang1 * $pi / 180.0);
-my $x2 = -1 * $rada * cos($ang2 * $pi / 180.0);
-my $z2 =      $rada * sin($ang2 * $pi / 180.0);
-
-#print "DEBUG: x1=$x1 z1=$z1 x2=$x2 z2=$z2\n";
-#exit;
+my $tl_length = 75.0 / 146.67;
 
 my $nfrq     = 10;
 my $fmhz     = 118.0;
@@ -31,13 +24,18 @@ my $nph = 31;
 my $dth = 360.0 / $nth;
 my $dph = 360.0 / $nph;
 
-print << "EOT";
+my $rada_x_2 = $rada * 2;
+my $middle_segment = int(($ns + 1) / 2);
+
+print <<EOT;
 CM  Simple loop antenna to locate interference using car's 2M radio.
 CE
-GA 1 $ns $rada $ang1 $ang2 $rad
-GW 2 1 $x1 0.00 $z1 $x2 0.00 $z2 $rad
+GA 2 $ns $rada $ang1 $ang2 $rad
+GM 0 0 0.00 0.00 0.00 0.00 0.00 $rada_x_2
+GW 1 1 -0.02 0.00 0.00 0.02 0.00 0.00 $rad
 GE
-EX 0 2 1 0 1.00 0.00 0.00 0.00 0.00 0.00
+EX 0 1 1 0 1.00 0.00 0.00 0.00 0.00 0.00
+TL 1 1 2 $middle_segment 75.00 $tl_length
 FR 0 $nfrq 0 0 $fmhz $delfrq 0.00 0.00 0.00 0.00
 RP 0 $nth $nph 1000 0.00 0.00 $dth $dph
 EN
